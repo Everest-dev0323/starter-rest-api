61fcec82b2acd570788001ae84edeef721ea4d1b
Everest-dev0323 <109219671+Everest-dev0323@users.noreply.github.com>
2023-04-29 10:24:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const CyclicDb = require("@cyclic.sh/dynamodb")
+const db = CyclicDb("wild-puce-sockeye-fezCyclicDB")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
